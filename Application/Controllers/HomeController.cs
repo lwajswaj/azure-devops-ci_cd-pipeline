292121c175b209e7026e6f057558854d1f3a0910
Leandro Wajswajn Pereyra <leandro.wajswajn.pereyra@gds.ey.com>
2019-09-25 22:00:50 -0300

    Added default message

--- Application/Controllers/HomeController.cs
+++ Application/Controllers/HomeController.cs
@@ -15,6 +15,13 @@ namespace Application.Controllers
       return View();
     }
 
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
+
+      return View();
+    }
+
     public IActionResult Contact()
     {
       ViewData["Message"] = "Your contact page.";
