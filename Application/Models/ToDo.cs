61354f9eadda7bce047869e95f2270d6b84d7fba
Leandro Wajswajn Pereyra <leandro.wajswajn.pereyra@gds.ey.com>
2019-09-25 22:11:06 -0300

    Model class created

---
+++ Application/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.Models
+{
+    public partial class ToDo
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Note { get; set; }
+    }
+}
